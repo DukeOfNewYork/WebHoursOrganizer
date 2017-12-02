9756de32351f25df82f1e7707e55c6e33f545fa4
DukeOfNewYork <simongraygit@gmail.com>
2017-12-02 11:03:48 -0500

    Uses tbody to store the autohours/manualhours disable functions instead of each individual row.

--- display.js
+++ display.js
@@ -1,16 +1,15 @@
-//Adds on change listeners to make sure manual hours and auto hours aren't active at the same time.
-var organizeInput = function() {
-    for (var count = 1; count <= lengthOfSchedule; count++) {
-        $('#' + count.toString()).change(function(){
-            $("#" + (this.id*10)).val(0);
-        });
-        $('#' + count*10).change(function(){
-            var name = this.id;
-            $('#' + (name/10)).prop('checked', false);
-        });
-    }
+//Adds on change listeners to the tbody, then checks if it's the upper row or lower row.
+//If manual hours or auto hours are changed it disables the other so that each aren't active at the same time.
+var organizeInput = function () {
+    $("#inputs tbody input").change(function (day) {
+        if (day.target.id <= lengthOfSchedule) {
+            $("#" + (day.target.id * 10)).val(0);
+        }
+        else {
+            $('#' + (day.target.id / 10)).prop('checked', false)
+        }
+    })
 };
-
 
 //Finalizes the organize hours array into a HTML table.
 function htmlDisplayPeople(peopleDictionary) {
